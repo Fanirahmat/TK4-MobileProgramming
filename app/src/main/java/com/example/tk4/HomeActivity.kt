54d92715474886ee255f9662f29d528d5a6cbae9
Hanavi Alvarel <hanavial@Hanavis-MacBook-Pro.local>
2023-07-30 21:28:04 +0700

    [FINISH] integrate google maps and fixing interface

--- app/src/main/java/com/example/tk4/HomeActivity.kt
+++ app/src/main/java/com/example/tk4/HomeActivity.kt
@@ -6,13 +6,21 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), OnMapReadyCallback {
 
     lateinit var textName: TextView
     lateinit var textEmail: TextView
 
     lateinit var btnLogout: Button
+
+    private lateinit var vMap: GoogleMap
 
     var firebaseAuth = FirebaseAuth.getInstance()
 
@@ -32,12 +40,24 @@ class HomeActivity : AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
-
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.mapView) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         btnLogout.setOnClickListener {
             firebaseAuth.signOut()
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        vMap = googleMap
+
+        val city = LatLng(13.7563, 100.5018)
+        vMap.addMarker(MarkerOptions()
+            .position(city)
+            .title("Marker in Bangkok"))
+        vMap.moveCamera(CameraUpdateFactory.newLatLngZoom(city, 5f))
     }
 }
